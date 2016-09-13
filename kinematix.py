08eb307f76dd6bbb6427362f882f2941f7981e12
Will Parker <will.parker0@gmail.com>
2016-09-13 08:19:09 +0000

    Not finished

--- kinematix.py
+++ kinematix.py
@@ -8,26 +8,35 @@ v_i and a_i. """
 #assume dt is always a given, so for any set of x, the dt between each 
 # x is given. aka, assume the set given to you is well formatted.
 
+x = []
 
-def kinematics(x, i, t):
+
+def kinematics(x, i, dt=1E-6):
     """Will calculate velocity and acceleration given well-formatted 
-    data set of positions."""
+    data set of positions and desired position."""
+    
 
-    if i<1 or i>(len(x)-1):
-        return 0
-    else: 
-        v = (x[i+1] - x[i-1])/(t[i+1]-t[i-1]) 
-        a = (2/(t[i+1]-t[i-1]))*((x[i+1]-x[i])/(t[i+1]-t[i]) - (x[i]-x[i-1])/(t[i]-t[i+1]))
-        return v, a
-        
+    v = []
+    a = []
+
+    for k in range(1, len(x)-2):
+        r = (x[k+1] - x[k-1])/(2.0*dt)
+        v.append(r)
+    print(v)
+        #a.append((1.0/(dt))*((x[k+1]-x[k])/(dt) - (x[k]-x[k-1])/(dt)))
+    #return v[i]
 
 def test_kinematics():
-    t = [0, .5, 1.5, 2.2]
-    x = []
-    k = range(len(t))
-    for i in k:
-        x.append(6*t[i])
-    return kinematics(x, 2, t)
+    """tests kinematics function across three time inputs (0, .5, 1.5, 2.2),
+    with x_i=Vt_i, where V is some constant. In this test, V=6. """
+    dt=1E-6
+    V=6
+
+    for i in range(int(.06/dt)+7):
+        x.append(V*i)
+
+    print(x)
+    #return kinematics(x, int(.5/dt)), kinematics(x, int(1.5/dt))
     
 
 print test_kinematics()
